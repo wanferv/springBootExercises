32186cd5951ffa3cfa33af6044bf1559e6375de1
wanferv <wanferv@gmail.com>
2024-09-24 23:21:26 -0500

    ejercicio con parametros mapeados

---
+++ cursoSpringBoot/src/main/java/cursoSpringBoot/controlles/GreetingRestController.java
@@ -0,0 +1,13 @@
+package cursoSpringBoot.controlles;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingRestController {
+    @GetMapping("/saludo/{name}")
+    public String greeting(@PathVariable String name){
+        return "Hola "+name;
+    }
+}
