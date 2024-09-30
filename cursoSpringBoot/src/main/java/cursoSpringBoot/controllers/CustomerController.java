9ab32f244841325e1d072dceb55e3d302aa473da
wanferv <wanferv@gmail.com>
2024-09-29 19:47:06 -0500

    ejercio con metodos POST

--- cursoSpringBoot/src/main/java/cursoSpringBoot/controllers/CustomerController.java
+++ cursoSpringBoot/src/main/java/cursoSpringBoot/controllers/CustomerController.java
@@ -1,9 +1,7 @@
 package cursoSpringBoot.controllers;
 
 import cursoSpringBoot.domain.Customer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -32,4 +30,11 @@ public class CustomerController {
         }
         return null;
     }
+
+    @PostMapping("/clientes")
+    public Customer postCliente(@RequestBody Customer customer){
+        customers.add(customer);
+        return customer;
+    }
+
 }
