a76ef902bc0eabc788ad21150c8d8e60313d9c9d
wanferv <wanferv@gmail.com>
2024-09-29 19:17:01 -0500

    ejercio busqueda de un cliente en particular

--- cursoSpringBoot/src/main/java/cursoSpringBoot/controllers/CustomerController.java
+++ cursoSpringBoot/src/main/java/cursoSpringBoot/controllers/CustomerController.java
@@ -2,6 +2,7 @@ package cursoSpringBoot.controllers;
 
 import cursoSpringBoot.domain.Customer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -12,13 +13,23 @@ import java.util.List;
 public class CustomerController {
     private List<Customer> customers = new ArrayList<>(Arrays.asList(
             new Customer(123,"Carlos Cuesta","carcues","contra1234"),
-            new Customer(456,"Mario Hernandez","","mariher456"),
+            new Customer(456,"Mario Hernandez","mherna","mariher456"),
             new Customer(789,"Laura Acuña","lauac","acu321"),
             new Customer(234,"Marcelo Perez","perlom","perlo3444")
     ));
 
-    @GetMapping("clientes")
+    @GetMapping("/clientes")
     public List<Customer> getCustomers() {
         return customers;
+    }
+    @GetMapping("/clientes/{username}")
+    public Customer getCustomer(@PathVariable String username){
+        for (Customer c:customers){
+            if (c.getUsername().equals(username)){
+                return c;
+            }
+
+        }
+        return null;
     }
 }
